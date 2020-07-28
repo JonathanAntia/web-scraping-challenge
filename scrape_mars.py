242676728d46d004d8c158ed2783bb2502e81b11
JonathanAntia <a_jon12@yahoo.com>
2020-07-28 11:19:40 -0500

    Scraping function

--- scrape_mars.py
+++ scrape_mars.py
@@ -8,7 +8,7 @@ def init_browser():
     return Browser('chrome',**executable_path, headless=False)
 
 def scrape():
-    browser = init_browser
+    browser = init_browser()
 
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
 
